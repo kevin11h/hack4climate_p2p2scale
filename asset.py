b92fd3d684b99b8fc0f3aeab442a1f1913985887
Derta Isyajora Rakhman <derta.spooz@gmail.com>
2017-11-14 16:13:30 +0100

    Add assets and sellers endpoint

--- asset.py
+++ asset.py
@@ -1,5 +1,12 @@
 from flask_restful import Resource
 
 class Asset(Resource):
+    def get(self):
+    	products = [{"id":1, "name":"Cocoa Pack A", "price" : 1000},
+    			{"id":2, "name":"Cocoa Pack B", "price" : 5000}]
+    	return {'response': 'ok', 'data' : products}
+
     def post(self):
-        return {'status': 'success'}
+    	all_data = request.get_json()
+    	return {'response': 'ok', 'data' : all_data}
+
