ed793c0cb6f35da57e2ffd863efbddbb9c04e50b
Derta Isyajora Rakhman <derta.spooz@gmail.com>
2017-11-14 17:40:41 +0100

    MongoDB Derta trial

---
+++ trial.py
@@ -0,0 +1,37 @@
+from pymongo import MongoClient
+
+DB_NAME = 'p2pscale'  
+DB_HOST = 'ds261745.mlab.com'
+DB_PORT = 61745
+DB_USER = 'test' 
+DB_PASS = '123456'
+
+connection = MongoClient(DB_HOST, DB_PORT)
+db = connection[DB_NAME]
+db.authenticate(DB_USER, DB_PASS)
+
+SEED_DATA = [
+    {
+        'decade': '1970s',
+        'artist': 'Debby Boone',
+        'song': 'You Light Up My Life',
+        'weeksAtOne': 10
+    },
+    {
+        'decade': '1980s',
+        'artist': 'Olivia Newton-John',
+        'song': 'Physical',
+        'weeksAtOne': 10
+    },
+    {
+        'decade': '1990s',
+        'artist': 'Mariah Carey',
+        'song': 'One Sweet Day',
+        'weeksAtOne': 16
+    }
+]
+# connect to the students database and the ctec121 collection
+
+user_data = {"name":"derta", "age":11}
+result = db.restaurants.insert_one(user_data)
+print(result.inserted_id)
