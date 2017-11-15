57de7abff02902551c3dbe055b896661a967b6c4
Christian Gorenflo <cgorenflo@uwaterloo.ca>
2017-11-15 10:44:45 +0100

    cors

--- hack4climate.py
+++ hack4climate.py
@@ -4,18 +4,17 @@ from asset import Asset
 from buyer import Buyer
 from seller import *
 import os
-from flask_cors import CORS
+from flask_restful.utils import cors
 
 
 
 app = Flask(__name__)
-CORS(app)
 api = Api(app)
 
 api.add_resource(Asset, '/Assets')
 api.add_resource(Seller, '/Sellers')
 api.add_resource(Buyer, '/Buyers')
-
+api.decorators=[cors.crossdomain(origin='*')]
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
